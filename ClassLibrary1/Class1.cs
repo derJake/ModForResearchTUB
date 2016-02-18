f2adfbb6c0e7b07ca82f3ced688884ae6ae143f5
Jacob Seifert <BastlerZ@gmail.com>
2016-02-18 11:56:18 +0100

    fix array index stuff and thereby enable logging

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -27,6 +27,21 @@ namespace ModForResearchTUB
         bool copsCalled = false;
         int currentCheckpoint = -1;
 
+        int lastMaxTimeSinceHitVehicle;
+        int lastMaxTimeSinceHitPed;
+        int lastMaxTimeSincePavement;
+        int lastMaxTimeSinceAgainstTraffic;
+
+        int numOfHitVehicles;
+        int numOfHitPeds;
+        int numOfTimesDrivingOnPavement;
+        int numOfTimesDrivingAgaingstTraffic;
+
+        int cumulativeTimeOnPavement;
+        int cumulativeTimeDrivingAgainstTraffic;
+        int raceStartTime;
+        int raceEndTime;
+
         Vector3 car_selection = new Vector3(-786.5052f, -2429.885f, 14.57072f);
         Vector3 car1_spawnpoint = new Vector3(-789.7347f, -2428.485f, 14.57072f);
         Vector3 car2_spawnpoint = new Vector3(-795.5708f, -2425.815f, 14.57072f);
@@ -65,8 +80,55 @@ namespace ModForResearchTUB
             if (race_started &&
                 playerInRaceCar) {
                 if (currentCheckpoint >= 0) {
-                    new UIResText(string.Format("currentCheckpoint is {0}", currentCheckpoint), new Point(Convert.ToInt32(res.Width) - safe.X - 180, Convert.ToInt32(res.Height) - safe.Y - 275), 0.3f, Color.White).Draw();
+                    new UIResText(string.Format("currentCheckpoint is {0}/{1}", currentCheckpoint, checkpoints.Length), new Point(Convert.ToInt32(res.Width) - safe.X - 180, Convert.ToInt32(res.Height) - safe.Y - 275), 0.3f, Color.White).Draw();
                 }
+
+                // logging some variables
+                int currentTimeSinceHitVehicle = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_HIT_VEHICLE, Game.Player);
+                int currentTimeSinceHitPed = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_HIT_PED, Game.Player);
+                int currentTimeSinceDrivingOnPavement = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_DROVE_ON_PAVEMENT, Game.Player);
+                int currentTimeSinceDrivingAgainstTraffic = Function.Call<int>(Hash.GET_TIME_SINCE_PLAYER_DROVE_AGAINST_TRAFFIC, Game.Player);
+
+                // if the timer was reset, there was a collision
+                if (currentTimeSinceHitVehicle < lastMaxTimeSinceHitVehicle) {
+                    numOfHitVehicles++;
+                }
+                // either way, save new timer
+                lastMaxTimeSinceHitVehicle = currentTimeSinceHitVehicle;
+
+                // if the timer was reset, there was a collision
+                if (currentTimeSinceHitPed < lastMaxTimeSinceHitPed)
+                {
+                    numOfHitPeds++;
+                }
+                // either way, save new timer
+                lastMaxTimeSinceHitPed = currentTimeSinceHitPed;
+
+                // player is currently driving on pavement
+                if (currentTimeSinceDrivingOnPavement == 0) {
+                    new UIResText("on pavement", new Point(Convert.ToInt32(res.Width) - safe.X - 180, Convert.ToInt32(res.Height) - safe.Y - 325), 0.3f, Color.OrangeRed).Draw();
+                }
+
+                // if the timer was reset, player drove on pavement
+                if (currentTimeSinceDrivingOnPavement < lastMaxTimeSincePavement)
+                {
+                    numOfTimesDrivingOnPavement++;
+                }
+                // either way, save new timer
+                lastMaxTimeSincePavement = currentTimeSinceDrivingOnPavement;
+
+                // player is currently driving against traffic
+                if (currentTimeSinceDrivingAgainstTraffic == 0) {
+                    new UIResText("against traffic", new Point(Convert.ToInt32(res.Width) - safe.X - 180, Convert.ToInt32(res.Height) - safe.Y - 350), 0.3f, Color.OrangeRed).Draw();
+                }
+
+                // if the timer was reset, player drove against traffic
+                if (currentTimeSinceDrivingAgainstTraffic < lastMaxTimeSinceAgainstTraffic)
+                {
+                    numOfTimesDrivingAgaingstTraffic++;
+                }
+                // either way, save new timer
+                lastMaxTimeSinceAgainstTraffic = currentTimeSinceDrivingAgainstTraffic;
             }
 
             if (Game.Player.Character.IsInVehicle()) {
@@ -75,27 +137,39 @@ namespace ModForResearchTUB
                 if (race_started && 
                     Game.Player.Character.IsInRangeOf(checkpoints[currentCheckpoint], 5f))
                 {
-                    UI.ShowSubtitle(string.Format("checkpoint {0} reached", currentCheckpoint + 1), 3000);
                     // finish race, if last checkpoint is reached
-                    if (currentCheckpoint == checkpoints.Length) {
-                        Function.Call(Hash.CLEAR_PLAYER_WANTED_LEVEL, Game.Player);
+                    if ((currentCheckpoint + 1) == checkpoints.Length) {
+                        UI.ShowSubtitle("Race finished!", 3000);
+                        Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Game.Player, 0, false);
+                        Function.Call(Hash.SET_PLAYER_WANTED_LEVEL_NOW, Game.Player, false);
+
+                        raceEndTime = Game.GameTime;
+
+                        writeRaceDataToLog();
                         clearStuffUp();
+                        return;
                     }
+
+                    UI.ShowSubtitle(string.Format("checkpoint {0}/{1} reached", currentCheckpoint + 1, checkpoints.Length), 3000);
 
                     // set next checkpoint
                     Function.Call(Hash.DELETE_CHECKPOINT, currentMarker);
                     currentCheckpoint++;
                     Vector3 coords = checkpoints[currentCheckpoint];
                     Vector3 nextCoords;
+                    int type;
                     if (currentCheckpoint < (checkpoints.Length - 1)) {
                         // if there are checkpoints left, get the next one's coordinates
                         nextCoords = checkpoints[currentCheckpoint + 1];
+                        type = 2;
                     } else {
+                        type = 14;
                         nextCoords = new Vector3(0,0,0);
+                        coords.Z = coords.Z + 3f;
                     }
                     
                     currentMarker = Function.Call<int>(Hash.CREATE_CHECKPOINT, 
-                        2, // type
+                        type, // type
                         coords.X,
                         coords.Y,
                         coords.Z - 1,
@@ -107,7 +181,7 @@ namespace ModForResearchTUB
                         155,     // G
                         0,        // B
                         100,    // Alpha
-                        0
+                        0 // number displayed in marker, if type is 42-44
                         );
 
                     currentBlip.Remove();
@@ -122,6 +196,7 @@ namespace ModForResearchTUB
                     if (!copsCalled) {
                         Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Game.Player, 3, false);
                         Function.Call(Hash.SET_PLAYER_WANTED_LEVEL_NOW, Game.Player, false);
+                        Game.Player.Character.CurrentVehicle.StartAlarm();
                         copsCalled = true;
                     }
                     playerInRaceCar = true;
@@ -132,11 +207,19 @@ namespace ModForResearchTUB
                     playerInRaceCar = true;
                 }
 
+                // start the race and set first marker + blip
                 if (playerInRaceCar &&
                     !race_started) {
                     race_started = true;
                     UI.ShowSubtitle("Race started!", 1250);
+
+                    // don't let player exit his racecar by conventional means
                     Game.DisableControl(0, GTA.Control.VehicleExit);
+
+                    // disable shooting from car?
+                    Game.DisableControl(0, GTA.Control.VehiclePassengerAim);
+
+                    // select the first checkpoint
                     Vector3 coords = checkpoints[currentCheckpoint];
                     Vector3 nextCoords;
                     if (currentCheckpoint < (checkpoints.Length - 1))
@@ -164,12 +247,11 @@ namespace ModForResearchTUB
                         );
                     currentBlip = World.CreateBlip(checkpoints[currentCheckpoint]);
                     Function.Call(Hash.SET_BLIP_ROUTE, currentBlip, true);
+
+                    raceStartTime = Game.GameTime;
                 }
                 
             }
-           
-            // Do something every tick here
-            //GTA.Native.Function.Call(Hash._SHOW_CURSOR_THIS_FRAME);
         }
 
         // KeyDown Event
@@ -223,9 +305,23 @@ namespace ModForResearchTUB
             Ped player = Game.Player.Character;
             player.Task.ClearAllImmediately(); // give back control to player
 
+            // teleport player and turn him towards cars
             Game.Player.Character.Position = car_selection;
             Game.Player.Character.Heading = car_spawn_player_heading;
 
+            // make player look at cars
+            Game.Player.Character.Task.StandStill(5000);
+            
+            UI.ShowSubtitle("slow car, good traction", 2500);
+            Game.Player.Character.Task.LookAt(car1_spawnpoint, 2500);
+            Wait(2500);
+            UI.ShowSubtitle("racecar (STEAL!)", 2500);
+            Game.Player.Character.Task.LookAt(car2_spawnpoint, 2500);
+            
+            // set up everything for logging
+            resetLoggingVariables();
+
+            // add some checkpoints for our race
             checkpoints = new Vector3[5];
             checkpoints[0] = race1Start;
             checkpoints[1] = new Vector3(-810.6682f, -2249.965f, 17.24915f);
@@ -234,7 +330,7 @@ namespace ModForResearchTUB
             checkpoints[4] = race1End;
             currentCheckpoint = 0;
 
-            //Function.Call(Hash.REQUEST_MODEL, VehicleHash.Buffalo);
+            // load the two models
             var vehicle1Model = new Model(VehicleHash.Buffalo);
             var vehicle2Model = new Model(VehicleHash.RapidGT);
             vehicle1Model.Request(500);
@@ -289,7 +385,7 @@ namespace ModForResearchTUB
                     255, // G
                     255, // B
                     100f, // distance
-                    2f, // brightness
+                    200f, // brightness
                     0.0f, // roundness
                     13f, // radius
                     1f // fadeout
@@ -320,7 +416,6 @@ namespace ModForResearchTUB
         }
 
         protected void clearStuffUp() {
-            UI.ShowSubtitle("clearing stuff up", 1250);
             Ped player = Game.Player.Character;
             player.Task.ClearAllImmediately(); // give back control to player
 
@@ -334,8 +429,45 @@ namespace ModForResearchTUB
             // clear map blip
             currentBlip.Remove();
 
+            // delete 3D marker
+            Function.Call(Hash.DELETE_CHECKPOINT, currentMarker);
+
             race_started = false;
             currentCheckpoint = -1;
+
+            resetLoggingVariables();
+            Wait(3000);
+            UI.ShowSubtitle("Everything reset", 3000);
+        }
+
+        protected void writeRaceDataToLog() {
+            Logger.Log(String.Format("race started: {0}", raceStartTime));
+            Logger.Log(String.Format("race ended: {0}", raceEndTime));
+            Logger.Log(String.Format("time taken: {0}", raceEndTime - raceStartTime));
+            Logger.Log(String.Format("Vehicle collisions: {0}", numOfHitVehicles));
+            Logger.Log(String.Format("Pedestrian collisions: {0}", numOfHitPeds));
+            Logger.Log(String.Format("Number of times player has driven against traffic: {0}", numOfTimesDrivingAgaingstTraffic));
+            Logger.Log(String.Format("Number of times player has driven against on pavement: {0}", numOfTimesDrivingOnPavement));
+            Logger.Log(String.Format("Cumulative time on pavement: {0}", cumulativeTimeOnPavement));
+            Logger.Log(String.Format("Cumulative time driving against traffic: {0}", cumulativeTimeDrivingAgainstTraffic));
+        }
+
+        protected void resetLoggingVariables() {
+            // reset logging variables
+            lastMaxTimeSinceHitVehicle = -1;
+            lastMaxTimeSinceHitPed = -1;
+            lastMaxTimeSincePavement = -1;
+            lastMaxTimeSinceAgainstTraffic = -1;
+            numOfHitVehicles = 0;
+            numOfHitPeds = 0;
+            numOfTimesDrivingOnPavement = 0;
+            numOfTimesDrivingAgaingstTraffic = 0;
+
+            raceStartTime = -1;
+            raceEndTime = -1;
+
+            cumulativeTimeDrivingAgainstTraffic = 0;
+            cumulativeTimeOnPavement = 0;
         }
 
         #endregion
