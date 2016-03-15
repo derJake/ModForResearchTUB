b1164bca0e650d69d2562c2fac8c7f09cc798ab4
Jacob Seifert <BastlerZ@gmail.com>
2016-03-15 17:23:41 +0100

    create interface for race objects

---
+++ ClassLibrary1/RaceInterface.cs
@@ -0,0 +1,20 @@
+﻿using GTA.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModForResearchTUB
+{
+    interface RaceInterface
+    {
+        void initRace();
+        void startRace();
+        void handleOnTick();
+        void finishRace();
+
+        void setCurrentCheckpoint(int index);
+        Vector3[] getCheckpoints();
+    }
+}
