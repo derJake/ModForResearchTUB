c74d3276e772c41c4eba40bb6830c04981379fdf
Jacob Seifert <BastlerZ@gmail.com>
2016-03-15 17:23:55 +0100

    + class for first race

---
+++ ClassLibrary1/RaceCarvsCar.cs
@@ -0,0 +1,44 @@
+﻿using GTA.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModForResearchTUB
+{
+    class RaceCarvsCar : RaceInterface
+    {
+        Vector3[] checkpoints;
+
+        public void finishRace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vector3[] getCheckpoints()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void handleOnTick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void initRace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setCurrentCheckpoint(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void startRace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
