894e559cc0c49a1a36f4e15fdd0c468a2f31acde
Jacob Seifert <BastlerZ@gmail.com>
2016-03-28 14:00:43 +0200

    reduce new race class to essentials

--- ClassLibrary1/RaceToWoodmill.cs
+++ ClassLibrary1/RaceToWoodmill.cs
@@ -14,22 +14,15 @@ namespace ModForResearchTUB
     class RaceToWoodmill : Script, RaceInterface
     {
         private Vector3[] checkpoints;
-        private int currentCheckpoint;
-        private bool playerInRaceCar = false;
 
         private int raceStartTime;
 
-        private String car_taken;
-
-        private List<Vehicle> vehicles = new List<Vehicle>();
+        private Vehicle raceVehicle;
 
         private Vector3 car_selection = new Vector3(-786.5052f, -2429.885f, 14.57072f);
         private Vector3 car1_spawnpoint = new Vector3(-789.7347f, -2428.485f, 14.57072f);
-        private Vector3 car2_spawnpoint = new Vector3(-795.5708f, -2425.815f, 14.57072f);
         private float car_spawn_heading = 147.0f;
         private float car_spawn_player_heading = 48.0f;
-        private Vector3 race1Start = new Vector3(-1015.348f, -2715.956f, 12.58948f);
-        private Vector3 race1End = new Vector3(-45.45972f, -784.222f, 44.34782f);
 
         public RaceToWoodmill() {
             // try and load this area already
@@ -43,9 +36,6 @@ namespace ModForResearchTUB
 
         public void finishRace()
         {
-            // log which car was taken
-            Logger.Log(car_taken);
-
             UI.ShowSubtitle(String.Format("Race finished! - Time: {0}s", (Game.GameTime - raceStartTime) / 1000), 3000);
             UI.Notify(String.Format("Race finished! - Time: {0}s", (Game.GameTime - raceStartTime) / 1000));
 
@@ -65,6 +55,7 @@ namespace ModForResearchTUB
                 Game.Player.Character.CurrentVehicle.HandbrakeOn = true;
             World.DestroyAllCameras();
             World.RenderingCamera = World.CreateCamera(GameplayCamera.Position, GameplayCamera.Rotation, 60f);
+            World.RenderingCamera.DepthOfFieldStrength = 200f;
 
             // play sounds
             Audio.PlaySoundFrontend("RACE_PLACED", "HUD_AWARDS");
@@ -79,12 +70,8 @@ namespace ModForResearchTUB
             Game.Player.Character.IsInvincible = false;
             Game.Player.CanControlCharacter = true;
 
-            foreach (Vehicle car in vehicles) {
-                car.MarkAsNoLongerNeeded();
-                car.Delete();
-            }
-
-            vehicles = new List<Vehicle>();
+            raceVehicle.MarkAsNoLongerNeeded();
+            raceVehicle.Delete();
         }
 
         public Vector3[] getCheckpoints()
@@ -99,18 +86,16 @@ namespace ModForResearchTUB
 
         public void initRace()
         {
-            UI.ShowSubtitle("initializing first race", 1250);
+            UI.ShowSubtitle("initializing woodmill track", 1250);
 
             // try to clear parking lot where cars are spawned
             // TO DO: check, if the boolean parameters have been documented
             // TO DO: spawn cars on the other side of the curb, to avoid false positives
-            Function.Call(Hash.CLEAR_ANGLED_AREA_OF_VEHICLES,
+            Function.Call(Hash.CLEAR_AREA_OF_VEHICLES,
                 car1_spawnpoint.X,
                 car1_spawnpoint.Y,
                 car1_spawnpoint.Z,
-                car2_spawnpoint.X,
-                car2_spawnpoint.Y,
-                car2_spawnpoint.Z,
+                50f,
                 false,
                 false,
                 false,
@@ -118,21 +103,11 @@ namespace ModForResearchTUB
                 false
             );
 
-            // try to load detailed terrain
-            Function.Call(Hash._SET_FOCUS_AREA,
-                car1_spawnpoint.X,
-                car1_spawnpoint.Y,
-                car1_spawnpoint.Z,
-                car2_spawnpoint.X,
-                car2_spawnpoint.Y,
-                car2_spawnpoint.Z
-             );
-
             // set time of day
-            World.CurrentDayTime = new TimeSpan(8, 30, 0);
+            World.CurrentDayTime = new TimeSpan(19, 15, 0);
 
             // set weather to rain
-            Function.Call(Hash.SET_WEATHER_TYPE_NOW_PERSIST, "RAIN");
+            Function.Call(Hash.SET_WEATHER_TYPE_NOW_PERSIST, "CLEARING");
 
             Ped player = Game.Player.Character;
             player.Task.ClearAllImmediately(); // give back control to player
@@ -161,74 +136,32 @@ namespace ModForResearchTUB
             checkpoints[15] = new Vector3(32.22876f, -773.1832f, 43.85289f);
             checkpoints[16] = new Vector3(-65.03999f, -725.1259f, 43.86914f);
             checkpoints[17] = new Vector3(-76.04148f, -749.7919f, 43.77972f);
-            checkpoints[18] = race1End;
-            currentCheckpoint = 0;
+            checkpoints[18] = new Vector3();
 
             // load the two models
             var vehicle1Model = new Model(VehicleHash.Buffalo);
-            var vehicle2Model = new Model(VehicleHash.RapidGT);
             vehicle1Model.Request(500);
-            vehicle2Model.Request(500);
 
             if (vehicle1Model.IsInCdImage &&
-                vehicle1Model.IsValid &&
-                vehicle2Model.IsInCdImage &&
-                vehicle2Model.IsValid
+                vehicle1Model.IsValid
                 )
             {
                 // If the model isn't loaded, wait until it is
-                while (!vehicle1Model.IsLoaded ||
-                        !vehicle2Model.IsLoaded)
+                while (!vehicle1Model.IsLoaded)
                     Script.Wait(100);
 
                 // create the slower, reliable car
-                var vehicle1 = World.CreateVehicle(VehicleHash.Buffalo, car1_spawnpoint, car_spawn_heading);
+                raceVehicle = World.CreateVehicle(VehicleHash.Buffalo, car1_spawnpoint, car_spawn_heading);
                 // create the racecar
-                var vehicle2 = World.CreateVehicle(VehicleHash.RapidGT, car2_spawnpoint, car_spawn_heading);
-                vehicles.Add(vehicle1);
-                vehicles.Add(vehicle2);
-
-                // open driver side door for player
-                Function.Call(Hash.SET_VEHICLE_DOOR_OPEN, vehicle1, 0, true, false);
-                Function.Call(Hash.SET_VEHICLE_ENGINE_ON, vehicle1, true, false, false);
-
-                // make fast vehicle locked, but able to break and enter
-                Function.Call(Hash.SET_VEHICLE_DOORS_LOCKED, vehicle2, 4);
-                Function.Call(Hash.SET_VEHICLE_NEEDS_TO_BE_HOTWIRED, vehicle2, true);
-                vehicle2.HasAlarm = true;
 
                 // make the fast one colorful, the other one white
-                Function.Call(Hash.SET_VEHICLE_CUSTOM_PRIMARY_COLOUR, vehicle1, 255, 255, 255);
-                Function.Call(Hash.SET_VEHICLE_CUSTOM_SECONDARY_COLOUR, vehicle1, 255, 255, 255);
+                Function.Call(Hash.SET_VEHICLE_CUSTOM_PRIMARY_COLOUR, raceVehicle, 255, 255, 255);
+                Function.Call(Hash.SET_VEHICLE_CUSTOM_SECONDARY_COLOUR, raceVehicle, 255, 255, 255);
 
-                Function.Call(Hash.SET_VEHICLE_CUSTOM_PRIMARY_COLOUR, vehicle2, 255, 0, 0);
-                Function.Call(Hash.SET_VEHICLE_CUSTOM_SECONDARY_COLOUR, vehicle2, 255, 50, 0);
-
-                Function.Call(Hash.SET_VEHICLE_INTERIORLIGHT, vehicle1, true);
-
-                // needs to be done onTick, me thinks
-                /*
-                Function.Call(
-                    Hash.DRAW_SPOT_LIGHT,
-                    car1_spawnpoint.X, // x
-                    car1_spawnpoint.Y, // y
-                    car1_spawnpoint.Z + 10f, // z
-                    0f, // direction x
-                    0f, // direction y
-                    -10f, // direction z, make it point downwards
-                    255, // R
-                    255, // G
-                    255, // B
-                    100f, // distance
-                    200f, // brightness
-                    0.0f, // roundness
-                    13f, // radius
-                    1f // fadeout
-                );*/
+                Function.Call(Hash.SET_VEHICLE_INTERIORLIGHT, raceVehicle, true);
             }
 
             vehicle1Model.MarkAsNoLongerNeeded();
-            vehicle2Model.MarkAsNoLongerNeeded();
 
             // make player look at cars
             Game.Player.Character.Task.StandStill(5000);
@@ -260,15 +193,6 @@ namespace ModForResearchTUB
                 90f
             );
 
-            // switch to this camera
-            Function.Call(Hash.RENDER_SCRIPT_CAMS, 1, 0, cam, 0, 0);
-            // play sound
-            Audio.PlaySoundFrontend("SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET");
-
-            UI.ShowSubtitle("Normal car with good traction", 2500);
-            Game.Player.Character.Task.LookAt(car2_spawnpoint, 2500);
-            Wait(2500);
-
             // switch back to main cam
             Function.Call(Hash.RENDER_SCRIPT_CAMS, 0, 1, cam, 0, 0);
 
@@ -277,7 +201,7 @@ namespace ModForResearchTUB
 
         public void setCurrentCheckpoint(int index)
         {
-            this.currentCheckpoint = index;
+            return;
         }
 
         public void startRace()
@@ -288,7 +212,7 @@ namespace ModForResearchTUB
 
             UI.ShowSubtitle("Race started!", 1250);
 
-            Game.Player.Character.CurrentVehicle.NumberPlate = "RACE 1";
+            Game.Player.Character.CurrentVehicle.NumberPlate = "RACE 2";
 
             raceStartTime = Game.GameTime;
         }
@@ -296,21 +220,7 @@ namespace ModForResearchTUB
         public bool checkRaceStartCondition()
         {
             // check which car player is using
-            if (Game.Player.Character.CurrentVehicle.Equals(vehicles[1]))
-            {
-                Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Game.Player, 3, false);
-                Function.Call(Hash.SET_PLAYER_WANTED_LEVEL_NOW, Game.Player, false);
-                vehicles[1].StartAlarm();
-                car_taken = "Player is in fast car";
-                playerInRaceCar = true;
-            }
-            if (Game.Player.Character.CurrentVehicle.Equals(vehicles[0]))
-            {
-                playerInRaceCar = true;
-                car_taken = "Player is in car with good traction";
-            }
-
-            return playerInRaceCar;
+            return Game.Player.Character.CurrentVehicle.Equals(raceVehicle);
         }
     }
 }
