2891d9dc8b9c87ae5c2e22deb8d066e7c0860521
Jacob Seifert <BastlerZ@gmail.com>
2016-04-11 13:42:43 +0200

    add static diagram class that only serves one purpose for now

---
+++ ClassLibrary1/DrawDiagram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ModForResearchTUB
+{
+    static class DrawDiagram
+    {
+        public static void renderDiagramToDisk(List<Tuple<int, int>> data)
+        {
+            using (Chart chart = new Chart() { Height = 1600, Width = 2400 })
+            {
+                // Konfiguration
+                chart.Titles.Add("Amount of Keypresses per length pressed");
+                chart.ChartAreas.Add(new ChartArea("statistic")
+                {
+                    AxisX = new Axis()
+                    {
+                        MajorGrid = new Grid() { Enabled = false }
+                    },
+                    AxisY = new Axis()
+                    {
+                        MajorGrid = new Grid() { LineColor = Color.LightGray, LineDashStyle = ChartDashStyle.Dot },
+                        Title = "amount"
+                    }
+                });
+                chart.Series.Add(new Series("data") { ChartType = SeriesChartType.Column });
+                // Daten
+                foreach (Tuple<int, int> entry in data) {
+                    chart.Series["data"].Points.Add(
+                        new DataPoint() {
+                            AxisLabel = entry.Item1.ToString(),
+                            YValues = new double[] { entry.Item2 }
+                        }
+                    );
+                }
+                // Ausgabe
+                chart.SaveImage(DateTime.Now.ToString("yyyy-MM-dd") + "dia.png", ChartImageFormat.Png);}
+            }
+        }
+    }
+}
