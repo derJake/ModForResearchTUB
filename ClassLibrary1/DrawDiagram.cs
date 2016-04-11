66df326ef36beb22ad50e6e5b9f3cadd20772f64
Jacob Seifert <BastlerZ@gmail.com>
2016-04-11 17:23:35 +0200

    switch back axes of diagram

--- ClassLibrary1/DrawDiagram.cs
+++ ClassLibrary1/DrawDiagram.cs
@@ -33,8 +33,8 @@ namespace ModForResearchTUB
                 foreach (Tuple<int, int> entry in data) {
                     chart.Series["data"].Points.Add(
                         new DataPoint() {
-                            AxisLabel = entry.Item2.ToString(),
-                            YValues = new double[] { entry.Item1 }
+                            AxisLabel = entry.Item1.ToString(),
+                            YValues = new double[] { entry.Item2 }
                         }
                     );
                 }
