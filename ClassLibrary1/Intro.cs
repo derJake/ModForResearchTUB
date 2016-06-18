7b22d33f18c62e81a52590ba7d995bf69cf1e020
Jacob Seifert <BastlerZ@gmail.com>
2016-06-18 11:33:02 +0200

    intro class for hopefully encapsulating some boilerplate

---
+++ ClassLibrary1/Intro.cs
@@ -0,0 +1,16 @@
+﻿using GTA;
+using GTA.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModForResearchTUB
+{
+    static class Intro
+    {
+        public static void showScene(Vector3 position, Vector3? rotation) {
+        }
+    }
+}
