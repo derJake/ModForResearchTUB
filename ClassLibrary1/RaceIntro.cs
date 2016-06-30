56c8e4393e19905070738048fed58f4e9a54442b
Jacob Seifert <BastlerZ@gmail.com>
2016-06-30 16:52:34 +0200

    change aggro car spawnpoint for driving over ped

--- ClassLibrary1/RaceIntro.cs
+++ ClassLibrary1/RaceIntro.cs
@@ -391,7 +391,7 @@ namespace ModForResearchTUB
             // run over pedestrian
 
             player.Position = new Vector3(-171, -1699.85f, 32.084f);
-            Vehicle aggro_car = createCarAt(VehicleHash.Buffalo, new Vector3(-169.1496f, -1698.676f, 31.59141f), 16.60919f);
+            Vehicle aggro_car = createCarAt(VehicleHash.Buffalo, new Vector3(-192.55f, -1674.355f, 33.092825f), 281.5312f);
 
             Game.Player.Character.Task.EnterVehicle(aggro_car, VehicleSeat.Driver, 10000, 2.0f, 16);
             Game.Player.Character.SetIntoVehicle(aggro_car, VehicleSeat.Driver);
