64c2ab773fb5b260a3b6654f5bd4a345b4ee65ee
Jacob Seifert <BastlerZ@gmail.com>
2016-07-04 11:52:04 +0200

    change additional police model to newer version

--- ClassLibrary1/RaceIntro.cs
+++ ClassLibrary1/RaceIntro.cs
@@ -523,9 +523,11 @@ namespace ModForResearchTUB
 
             Game.Player.WantedLevel = 4;
 
-            createCarAt(VehicleHash.Police, new Vector3(-162.3957f, -1666.599f, 32.63364f), 310);
-            createCarAt(VehicleHash.Police, new Vector3(-146.7052f, -1641.89f, 32.41359f), 120);
-            createCarAt(VehicleHash.Police, new Vector3(-143.4058f, -1645.468f, 32.21568f), 158.9681f);
+            VehicleHash police_car_model = VehicleHash.Police2;
+
+            createCarAt(police_car_model, new Vector3(-162.3957f, -1666.599f, 32.63364f), 310);
+            createCarAt(police_car_model, new Vector3(-146.7052f, -1641.89f, 32.41359f), 120);
+            createCarAt(police_car_model, new Vector3(-143.4058f, -1645.468f, 32.21568f), 158.9681f);
 
             List<Ped> additional_police = new List<Ped>(6);
             Ped policeman_3 = createPedAt(PedHash.Cop01SMY, new Vector3(-142.0587f, -1645.612f, 32.63714f));
