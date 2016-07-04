f94d5368a5134466c6849ddd1f929975b94fdd5d
Jacob Seifert <BastlerZ@gmail.com>
2016-07-04 11:39:18 +0200

    have wanted level flash through following scene

--- ClassLibrary1/RaceIntro.cs
+++ ClassLibrary1/RaceIntro.cs
@@ -521,11 +521,11 @@ namespace ModForResearchTUB
 
             Wait(regularIntroSceneLength);
 
-            Function.Call(Hash.FLASH_WANTED_DISPLAY, false);
-
             createCarAt(VehicleHash.Police, new Vector3(-162.3957f, -1666.599f, 32.63364f), 310);
             createCarAt(VehicleHash.Police, new Vector3(-146.7052f, -1641.89f, 32.41359f), 120);
             createCarAt(VehicleHash.Police, new Vector3(-143.4058f, -1645.468f, 32.21568f), 158.9681f);
+
+            Function.Call(Hash.FLASH_WANTED_DISPLAY, false);
 
             // remove wanted level and aggressive cop peds
             World.SetRelationshipBetweenGroups(Relationship.Neutral, playerRGroup, copHash);
