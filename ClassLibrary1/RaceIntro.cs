e90e4463c50cc23bd0848e6100f582434511292c
Jacob Seifert <BastlerZ@gmail.com>
2016-07-28 12:41:14 +0200

    create ped and car for reverse driving and give task to ped

--- ClassLibrary1/RaceIntro.cs
+++ ClassLibrary1/RaceIntro.cs
@@ -730,17 +730,20 @@ namespace ModForResearchTUB
             desert_car.Heading = 199.2738f;
             desert_car.Position = new Vector3(-1043.221f, 385.1127f, 69.23925f);
 
+            Ped reverse_driver = ut.createPedAt(PedHash.AfriAmer01AMM, new Vector3(-998.9263f, 373.3408f, 71.93969f));
+            Vehicle reverse_car = ut.createCarAt(VehicleHash.Surge, new Vector3(-998.9263f, 373.3408f, 71.93969f), 237.6931f);
+            reverse_driver.SetIntoVehicle(reverse_car, VehicleSeat.Driver);
+
             bmsg.ShowOldMessage(rm.GetString("intro19"), regularIntroSceneLength);
 
             Wait(3000);
-            UI.ShowSubtitle("reverse parking");
             Vector3 destination = new Vector3(-1007, 368.8f, 71.8f);
             //player.Task.ParkVehicle(car, new Vector3(-1007, 368.8f, 71.8f), 311);
             //Function.Call(Hash.TASK_VEHICLE_PARK, desert_car, -1007, 368.8f, 71.8f, 311, 2, 0, true);
 
             Function.Call(Hash.TASK_VEHICLE_GOTO_NAVMESH,
-                player,
-                desert_car,
+                reverse_driver,
+                reverse_car,
                 destination.X,
                 destination.Y,
                 destination.Z,
