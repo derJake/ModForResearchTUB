a9365cdf5ef83772f761677fdf783a79e328f9d3
Jacob Seifert <BastlerZ@gmail.com>
2016-07-28 12:45:16 +0200

    remove task, because of setintovehicle

--- ClassLibrary1/RaceIntro.cs
+++ ClassLibrary1/RaceIntro.cs
@@ -185,7 +185,6 @@ namespace ModForResearchTUB
             raceVehicle = ut.createCarAt(vehicleHash, car1_spawnpoint, car_spawn_heading);
 
             // make player enter vehicle
-            Game.Player.Character.Task.EnterVehicle(raceVehicle, VehicleSeat.Driver, 10000, 2.0f, 16);
             Game.Player.Character.SetIntoVehicle(raceVehicle, VehicleSeat.Driver);
 
             // set time of day
