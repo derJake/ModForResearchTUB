055ad1b67c46c5eaa85aadb7405be206cfb46b0d
Jacob Seifert <BastlerZ@gmail.com>
2016-08-01 19:05:17 +0200

    fix comment

--- ClassLibrary1/ModForResearchTUB.cs
+++ ClassLibrary1/ModForResearchTUB.cs
@@ -1492,7 +1492,7 @@ namespace ModForResearchTUB
         protected void buildMenu() {
             myMenu = new UIMenu("Mod4ResearchTUB", "~b~meh");
 
-            // checkbox for schema designer
+            // checkbox for route designer
             var route_designer_checkbox = new UIMenuCheckboxItem("Route Designer", route_designer_active, "Activate the route designer?");
             myMenu.AddItem(route_designer_checkbox);
             myMenu.RefreshIndex();
