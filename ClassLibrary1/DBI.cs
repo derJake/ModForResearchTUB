82df183a1960929b52a4ab8169e892b483cb444e
Jacob Seifert <BastlerZ@gmail.com>
2016-08-18 11:14:35 +0200

    add DB interface class with some method stubs

---
+++ ClassLibrary1/DBI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ModForResearchTUB
+{
+    class DBI
+    {
+        public void createDataset(String participant_name) {
+
+        }
+
+        public void insertValue(String attribute_key, double value) {
+
+        }
+
+        private void createAttribute(String attribute_key) {
+
+        }
+
+        public int getAttributeId(String attribute_key) {
+            return 0;
+        }
+
+        private void createScheme() {
+            createDataSetTable();
+            createAttributeKeyTable();
+            createAttributeValueTable();
+        }
+
+        #region internal
+
+        private void createDataSetTable() {
+
+        }
+
+        private void createAttributeKeyTable() {
+        }
+
+        private void createAttributeValueTable() {
+
+        }
+
+        #endregion 
+    }
+}
