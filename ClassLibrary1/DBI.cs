b30058df0e66418713ceba7eff4823cc389cb195
Jacob Seifert <BastlerZ@gmail.com>
2016-08-25 09:58:50 +0200

    convert SQLite code to SQL Server interface

--- ClassLibrary1/DBI.cs
+++ ClassLibrary1/DBI.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
+using System.Data.SqlClient;
 using System.Globalization;
 
 namespace ModForResearchTUB
 {
     class DBI
     {
-        SQLiteConnection m_dbConnection;
+        SqlConnection m_dbConnection;
 
         public DBI() {
             m_dbConnection =
-            new SQLiteConnection("Data Source=mod4researchTUB.db;Version=3;");
+            new SqlConnection("user id=TUBMod4Research;" +
+                                "password=123456;server=localhost;" +
+                                "Trusted_Connection=yes;" +
+                                "database=TUBMod4Research; " +
+                                "connection timeout=30");
 
             if (m_dbConnection != null)
             {
@@ -32,7 +35,7 @@ namespace ModForResearchTUB
             }
         }
 
-        public long createDataset(String participant_name) {
+        public int createDataset(String participant_name) {
             // open DB connection
             m_dbConnection.Open();
 
@@ -40,8 +43,8 @@ namespace ModForResearchTUB
             DateTime current_date = new DateTime();
 
             // create the actual insert query and set parameters
-            SQLiteCommand insertSQL = new SQLiteCommand(
-                "INSERT INTO data_set (participant_name, date) VALUES (?,?)", m_dbConnection);
+            SqlCommand insertSQL = new SqlCommand(
+                "INSERT INTO data_set (participant_name, date) OUTPUT INSERTED.ID VALUES (?,?)", m_dbConnection);
             insertSQL.Parameters.Add(participant_name);
             insertSQL.Parameters.Add(current_date.ToString("yyyy-MM-dd HH:mm:ss",
                   CultureInfo.InvariantCulture));
@@ -50,10 +53,7 @@ namespace ModForResearchTUB
             try
             {
                 // data set insert was successful
-                if (insertSQL.ExecuteNonQuery() == 1) {
-                    return m_dbConnection.LastInsertRowId;
-                }
-
+                return (Int32)insertSQL.ExecuteScalar();
             }
             catch (Exception ex)
             {
@@ -63,8 +63,6 @@ namespace ModForResearchTUB
                 // close DB connection
                 m_dbConnection.Close();
             }
-
-            return 0;
         }
 
         public void insertValue(String attribute_key, double value) {
@@ -158,7 +156,7 @@ namespace ModForResearchTUB
             m_dbConnection.Open();
 
             // table creation query object
-            SQLiteCommand creationSQL = new SQLiteCommand(sql);
+            SqlCommand creationSQL = new SqlCommand(sql);
 
             // execute query
             try
