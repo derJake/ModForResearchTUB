28dc527ac7510b6781de85abfae89a6c4fa228cc
Jacob Seifert <BastlerZ@gmail.com>
2016-09-18 13:12:29 +0200

    remove obsolete comment

--- ClassLibrary1/RaceConvoy.cs
+++ ClassLibrary1/RaceConvoy.cs
@@ -231,8 +231,6 @@ namespace ModForResearchTUB
 
             //cam.PointAt(raceVehicle);
 
-            // TO DO: move camera around
-
             // switch to this camera
             Function.Call(Hash.RENDER_SCRIPT_CAMS, 1, 0, cam, 0, 0);
             // play sound
